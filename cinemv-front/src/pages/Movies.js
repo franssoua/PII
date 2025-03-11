af8bf86eafe1bcc4497a53fa50eb80310f8fa645
franssoua <frobilliard@ensc.fr>
2025-03-11 20:09:43 +0100

    affichage film css

--- cinemv-front/src/pages/Movies.js
+++ cinemv-front/src/pages/Movies.js
@@ -22,20 +22,29 @@ function Movies() {
 
   return (
     <Container>
-      <Typography variant="h3" gutterBottom>
+      <Typography variant="h3" gutterBottom align="center">
         Au cinéma en ce moment
       </Typography>
-      <Grid container spacing={3}>
+      <Grid container spacing={2} justifyContent="center">
         {movies.map((movie) => (
-          <Grid item key={movie.id} xs={12} sm={6} md={4} lg={3}>
-            <Card>
+          <Grid item key={movie.id} xs={12} sm={6} md={4} lg={2.1}>
+            <Card
+              sx={{
+                maxWidth: 180,
+                height: 310,
+                mx: "auto",
+                transition: "0.3s",
+                "&:hover": { transform: "scale(1.05)" },
+              }}
+            >
               <CardMedia
                 component="img"
                 image={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
                 alt={movie.title}
+                sx={{ height: 250 }}
               />
-              <CardContent>
-                <Typography variant="h6">{movie.title}</Typography>
+              <CardContent sx={{ textAlign: "center", padding: "8px" }}>
+                <Typography variant="subtitle2">{movie.title}</Typography>
               </CardContent>
             </Card>
           </Grid>
